35a711b616f79aedc7e908ff566f08e9e74b64d6
Jaewon Lee <joyjwlee@gmail.com>
2020-02-18 15:20:29 -0800

    Finished 7

---
+++ 7. Reverse Integer.cpp
@@ -0,0 +1,24 @@
+class Solution
+{
+public:
+    int reverse(int x)
+    {
+        int digit, ans = 0;
+        while (x)
+        {
+            if (ans > 0 && ans > INT_MAX / 10)
+                return 0;
+            if (ans < 0 && ans < INT_MIN / 10)
+                return 0;
+            ans *= 10;
+            int digit = x % 10;
+            if (ans > 0 && ans > INT_MAX - digit)
+                return 0;
+            if (ans < 0 && ans < INT_MIN - digit)
+                return 0;
+            ans += digit;
+            x /= 10;
+        }
+        return ans;
+    }
+};
