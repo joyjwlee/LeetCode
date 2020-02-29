b36a447e123427f62e92bcef1ed98ce240acbd53
Jaewon Lee <joyjwlee@gmail.com>
2020-02-29 12:03:04 -0800

    Finished Problem 14

---
+++ Problems/14. Longest Common Prefix.cpp
@@ -0,0 +1,16 @@
+class Solution
+{
+public:
+    string longestCommonPrefix(vector<string> &strs)
+    {
+        if (strs.empty())
+            return "";
+        if (strs.size() == 1)
+            return strs[0];
+        sort(strs.begin(), strs.end());
+        int count = 0;
+        while (count < strs[0].size() && strs[0][count] == strs[strs.size() - 1][count])
+            count++;
+        return count == 0 ? "" : strs[0].substr(0, count);
+    }
+};
