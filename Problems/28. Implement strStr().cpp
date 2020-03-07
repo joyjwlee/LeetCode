a971e6f5b79037f2a6ef62a648eb9bcba7ee136a
Jaewon Lee <joyjwlee@gmail.com>
2020-03-07 10:28:24 -0800

    Finished Problem 28

---
+++ Problems/28. Implement strStr().cpp
@@ -0,0 +1,32 @@
+class Solution
+{
+public:
+    int strStr(string haystack, string needle)
+    {
+        if (needle.length() == 0)
+            return 0;
+        for (int i = 0; i < haystack.length(); i++)
+        {
+            if (haystack[i] == needle[0])
+            {
+                bool match = true;
+                if (i + needle.length() - 1 <= haystack.length() - 1)
+                {
+                    for (int j = 1; j < needle.length(); j++)
+                    {
+                        if (haystack[i + j] != needle[j])
+                        {
+                            match = false;
+                            break;
+                        }
+                    }
+                    if (match)
+                        return i;
+                }
+                else
+                    return -1;
+            }
+        }
+        return -1;
+    }
+};
