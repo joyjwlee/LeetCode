f4a1efa16d8bf9768addf4e9df558244709063e6
Jaewon Lee <joyjwlee@gmail.com>
2020-03-23 20:10:59 -0700

    Solved Problem 50

---
+++ Problems/53. Maximum Subarray.cpp
@@ -0,0 +1,15 @@
+class Solution
+{
+public:
+    int maxSubArray(vector<int> &nums)
+    {
+        int maxSumAtIndex = nums[0];
+        int maxTillIndex = nums[0];
+        for (int i = 1; i < nums.size(); i++)
+        {
+            maxSumAtIndex = max(maxSumAtIndex + nums[i], nums[i]);
+            maxTillIndex = max(maxTillIndex, maxSumAtIndex);
+        }
+        return maxTillIndex;
+    }
+}; //Kadane's algorithm!
