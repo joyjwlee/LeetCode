6b9630e4fae5b2e08a5a1f545cadf17ed250169a
Jaewon Lee <joyjwlee@gmail.com>
2020-05-04 08:16:38 -0700

    solved problem 11

--- Problems/11. Container With Most Water.cpp
+++ Problems/11. Container With Most Water.cpp
@@ -1,21 +1,18 @@
-class Solution
-{
+class Solution {
 public:
-    int maxArea(vector<int> &height)
-    {
+    int maxArea(vector<int> &height) {
         int maximum = 0;
-
         int first = 0;
         int last = height.size() - 1;
 
-        while (first < last)
-        {
+        while (first < last) {
             maximum = max((min(height[first], height[last]) * (last - first)), maximum);
             if (height[first] < height[last])
                 first++;
             else
                 last--;
         }
+
         return maximum;
-    }
+  }
 };
