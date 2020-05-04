9625936616f6517a2ad5d6f57f0c35949279f2cd
Jaewon Lee <joyjwlee@gmail.com>
2020-05-04 08:18:56 -0700

    solved problem 9

--- Problems/9. Palindrome Number.cpp
+++ Problems/9. Palindrome Number.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-    if (x < 0)
-        return false;
-    else {
-        long long n, digit, rev = 0;
-        n = x;
-        do {
-            digit = x % 10;
-            rev = (rev * 10) + digit;
-            x = x / 10;
-        } while (x != 0);
-        if (rev == n)
-            return true;
-        return false;
+        if (x < 0)
+            return false;
+        else {
+            long long n, digit, rev = 0;
+            n = x;
+            do {
+                digit = x % 10;
+                rev = (rev * 10) + digit;
+                x = x / 10;
+            } while (x != 0);
+            if (rev == n)
+                return true;
+            return false;
+        }
     }
-  }
 };
