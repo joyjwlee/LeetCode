45bef3402844186c504f4f79ee3a07bb8ba21596
Jaewon Lee <joyjwlee@gmail.com>
2020-05-06 08:19:06 -0700

    solved problem 28

--- Problems/28. Implement strStr().cpp
+++ Problems/28. Implement strStr().cpp
@@ -1,30 +1,24 @@
-class Solution
-{
+class Solution {
 public:
-    int strStr(string haystack, string needle)
-    {
+    int strStr(string haystack, string needle) {
         if (needle.length() == 0)
             return 0;
-        for (int i = 0; i < haystack.length(); i++)
-        {
-            if (haystack[i] == needle[0])
-            {
+        for (int i = 0; i < haystack.length(); i++) {
+            if (haystack[i] == needle[0]) {
                 bool match = true;
-                if (i + needle.length() - 1 <= haystack.length() - 1)
-                {
-                    for (int j = 1; j < needle.length(); j++)
-                    {
-                        if (haystack[i + j] != needle[j])
-                        {
+                if (i + needle.length() - 1 <= haystack.length() - 1) {
+                    for (int j = 1; j < needle.length(); j++) {
+                        if (haystack[i + j] != needle[j]) {
                             match = false;
                             break;
                         }
                     }
-                    if (match)
+                    if (match) {
                         return i;
-                }
-                else
+                    }
+                } else {
                     return -1;
+                }
             }
         }
         return -1;
