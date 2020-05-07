bab66785eb8ee53dd0dd82b471c1f42b7e25226a
Jaewon Lee <joyjwlee@gmail.com>
2020-05-07 08:30:40 -0700

    solved problem 50

--- Problems/50. Pow(x, n).cpp
+++ Problems/50. Pow(x, n).cpp
@@ -1,8 +1,6 @@
-class Solution
-{
+class Solution {
 public:
-    double myPow(double x, int n)
-    {
+    double myPow(double x, int n) {
         double ans = 1;
         if (x == 1)
             return 1;
@@ -15,14 +13,12 @@ public:
             ans = ans / x;
         long m = n;
         m = abs(m);
-        while (m / (order * 2))
-        {
+        while (m / (order * 2)) {
             ans *= ans;
             order *= 2;
         }
         rem = m - order;
-        for (int i = 0; i < rem; i++)
-        {
+        for (int i = 0; i < rem; i++) {
             if (n > 0)
                 ans *= x;
             if (n < 0)
