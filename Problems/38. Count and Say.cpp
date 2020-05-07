cfaa13b3a61da00c785836accf30a10f2af58b94
Jaewon Lee <joyjwlee@gmail.com>
2020-05-07 08:35:09 -0700

    solved problem 38

--- Problems/38. Count and Say.cpp
+++ Problems/38. Count and Say.cpp
@@ -1,17 +1,13 @@
-class Solution
-{
+class Solution {
 public:
-    string countAndSay(int n)
-    {
+    string countAndSay(int n) {
         if (n == 1)
             return "1";
         string str = countAndSay(n - 1);
         string ans = "";
-        for (int i = 0; i < str.size(); i++)
-        {
+        for (int i = 0; i < str.size(); i++) {
             int count = 1;
-            while (i < str.size() - 1 && str[i] == str[i + 1])
-            {
+            while (i < str.size() - 1 && str[i] == str[i + 1]) {
                 i++;
                 count++;
             }
