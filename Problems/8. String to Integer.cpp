99d7b5c81540220ff90a28a3f89386ff6c06b5e8
Jaewon Lee <joyjwlee@gmail.com>
2020-05-10 14:00:00 -0700

    solved problem 8

--- Problems/8. String to Integer.cpp
+++ Problems/8. String to Integer.cpp
@@ -1,12 +1,12 @@
 class Solution {
   public:
     int myAtoi(string str) {
-        long long n = 0;
-        sscanf(str.c_str(), "%lld", &n);
-        if (n > INT_MAX)
+        long long ans = 0;
+        sscanf(str.c_str(), "%lld", &ans);
+        if (ans > INT_MAX)
             return INT_MAX;
-        if (n < INT_MIN)
+        if (ans < INT_MIN)
             return INT_MIN;
-        return (int)n;
+        return (int)ans;
     }
 };
