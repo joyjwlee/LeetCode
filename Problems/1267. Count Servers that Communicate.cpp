767395ae9b74810b061f9435ad81fbb25c84a39b
Jaewon Lee <joyjwlee@gmail.com>
2020-05-10 12:20:36 -0700

    solved problem 1267

--- Problems/1267. Count Servers that Communicate.cpp
+++ Problems/1267. Count Servers that Communicate.cpp
@@ -1,24 +1,26 @@
-class Solution
-{
+class Solution {
 public:
-    int countServers(vector<vector<int>> &grid)
-    {
+    int countServers(vector<vector<int>> &grid) {
         unordered_map<int, int> hc, vc;
         int ans = 0;
 
-        for (int i = 0; i < grid.size(); ++i)
-            for (int j = 0; j < grid[0].size(); ++j)
-                if (grid[i][j])
-                {
+        for (int i = 0; i < grid.size(); ++i) {
+            for (int j = 0; j < grid[0].size(); ++j) {
+                if (grid[i][j]) {
                     hc[i]++;
                     vc[j]++;
                     ans++;
                 }
+            }
+        }
 
-        for (int i = 0; i < grid.size(); ++i)
-            for (int j = 0; j < grid[0].size(); ++j)
-                if (grid[i][j] && hc[i] == 1 && vc[j] == 1)
+        for (int i = 0; i < grid.size(); ++i) {
+            for (int j = 0; j < grid[0].size(); ++j) {
+                if (grid[i][j] && hc[i] == 1 && vc[j] == 1) {
                     ans--;
+                }
+            }
+        }
         return ans;
     }
 };
