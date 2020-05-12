15f78d6f5c9c3cfb4c32acc5a8546d6e62964cb4
Jaewon Lee <joyjwlee@gmail.com>
2020-05-12 07:18:10 -0700

    solved problem 200

--- Problems/200. Number of Islands.cpp
+++ Problems/200. Number of Islands.cpp
@@ -1,10 +1,7 @@
-class Solution
-{
+class Solution {
 public:
-    int numIslands(vector<vector<char>> &grid)
-    {
-        if (grid.empty())
-        {
+    int numIslands(vector<vector<char>> &grid) {
+        if (grid.empty()) {
             return 0;
         }
 
@@ -15,40 +12,32 @@ public:
 
         stack<pair<int, int>> s;
 
-        for (int i = 0; i < R; i++)
-        {
-            for (int j = 0; j < C; j++)
-            {
-                //if we come across one
-                if (grid[i][j] == '1')
-                {
-                    //take care of it
+        for (int i = 0; i < R; i++) {
+            for (int j = 0; j < C; j++) {
+                // if we come across one
+                if (grid[i][j] == '1') {
+                    // take care of it
                     ans++;
                     grid[i][j] = '0';
                     s.push(pair(i, j));
-                    //remove all adjecent neighbors
-                    while (!s.empty())
-                    {
-                        pair rc = s.top();
+                    // remove all adjecent neighbors
+                    while (!s.empty()) {
+                        pair curr = s.top();
                         s.pop();
-                        int r = rc.first, c = rc.second;
-                        if (r + 1 < R && grid[r + 1][c] == '1')
-                        {
+                        int r = curr.first, c = curr.second;
+                        if (r + 1 < R && grid[r + 1][c] == '1') {
                             s.push(pair(r + 1, c));
                             grid[r + 1][c] = '0';
                         }
-                        if (r - 1 >= 0 && grid[r - 1][c] == '1')
-                        {
+                        if (r - 1 >= 0 && grid[r - 1][c] == '1') {
                             s.push(pair(r - 1, c));
                             grid[r - 1][c] = '0';
                         }
-                        if (c + 1 < C && grid[r][c + 1] == '1')
-                        {
+                        if (c + 1 < C && grid[r][c + 1] == '1') {
                             s.push(pair(r, c + 1));
                             grid[r][c + 1] = '0';
                         }
-                        if (c - 1 >= 0 && grid[r][c - 1] == '1')
-                        {
+                        if (c - 1 >= 0 && grid[r][c - 1] == '1') {
                             s.push(pair(r, c - 1));
                             grid[r][c - 1] = '0';
                         }
