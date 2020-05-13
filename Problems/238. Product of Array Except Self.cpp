347d2bb6e363a5400dde17498cb0da679612edcc
Jaewon Lee <joyjwlee@gmail.com>
2020-05-13 07:42:51 -0700

    almost there

--- Problems/238. Product of Array Except Self.cpp
+++ Problems/238. Product of Array Except Self.cpp
@@ -1,13 +1,10 @@
-class Solution
-{
+class Solution {
 public:
-    vector<int> productExceptSelf(vector<int> &nums)
-    {
+    vector<int> productExceptSelf(vector<int> &nums) {
         int product = 1;
         int zeroes = 0;
         int n = nums.size();
-        for (auto x : nums)
-        {
+        for (auto x : nums) {
             if (x == 0)
                 zeroes++;
             else
@@ -15,30 +12,22 @@ public:
         }
 
         vector<int> ans;
-        if (zeroes > 1)
-        {
+        if (zeroes > 1) {
             return vector<int>(n, 0);
-        }
-        else if (zeroes == 1)
-        {
-            for (auto x : nums)
-            {
+        } else if (zeroes == 1) {
+            for (auto x : nums) {
                 if (x == 0)
                     ans.push_back(product);
                 else
                     ans.push_back(0);
             }
-        }
-        else
-        {
+        } else {
             vector<int> ans(n, 1);
-            for (int i = 1; i < n; i++)
-            {
+            for (int i = 1; i < n; i++) {
                 ans[i] = ans[i - 1] * nums[i - 1];
             }
             int p = 1;
-            for (int i = n - 2; i >= 0; i--)
-            {
+            for (int i = n - 2; i >= 0; i--) {
                 p *= nums[i + 1];
                 ans[i] *= p;
             }
