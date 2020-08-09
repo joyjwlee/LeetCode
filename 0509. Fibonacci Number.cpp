7a799bd12c588da563662d5a66ec32f54466c949
Jaewon Lee <joyjwlee@gmail.com>
2020-08-09 10:41:12 -0700

    Fibonacci Number

--- 0509. Fibonacci Number.cpp
+++ 0509. Fibonacci Number.cpp
@@ -1,7 +1,7 @@
 class Solution {
   public:
     int fib(int N) {
-        // can do dp, just do for loop
+        // edge case
         if (N == 0 || N == 1)
             return N;
         // dp through
