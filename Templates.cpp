9c19fc4d56997bf29a137c24f02ad8822d1e5d33
Jaewon Lee <joyjwlee@gmail.com>
2020-08-24 22:45:13 -0700

    DFS

--- Templates.cpp
+++ Templates.cpp
@@ -22,3 +22,24 @@ sort(points.begin(), points.end(), [](vector<int> &a, vector<int> &b) {
 
 // UNORDERED MAP
 unordered_map<char, int> mp;
+
+// DFS
+// dr dc arrays
+int dr[4] = {-1, 0, 1, 0};
+int dc[4] = {0, 1, 0, -1};
+
+// check for boundary
+bool shouldVisit(vector<vector<char>> &grid, int i, int j) {
+    if (0 <= i && i < grid.size())
+        if (0 <= j && j < grid[0].size())
+            return true;
+    return false;
+}
+
+void DFS(vector<vector<char>> &grid, int i, int j) {
+    grid[i][j] = 0;
+    for (int k = 0; k < 4; k++) {
+        if (shouldVisit(grid, i + dr[k], j + dc[k]))
+            DFS(grid, i + dr[k], j + dc[k]);
+    }
+}
