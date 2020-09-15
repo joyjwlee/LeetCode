dc3696933148eba96d4b8dc69d39a60cd4430141
Jaewon Lee <joyjwlee@gmail.com>
2020-09-15 09:59:29 -0700

    Three Consecutive Odds

---
+++ 1550. Three Consecutive Odds.cpp
@@ -0,0 +1,15 @@
+class Solution {
+  public:
+    bool threeConsecutiveOdds(vector<int> &arr) {
+        int cnt = 0;
+        for (int n : arr) {
+            if (n % 2 == 1)
+                cnt++;
+            else
+                cnt = 0;
+            if (cnt == 3)
+                return true;
+        }
+        return false;
+    }
+};
