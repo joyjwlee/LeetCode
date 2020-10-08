a1a2b2850a34972f8374f1f75a68f3274d736a38
Jaewon Lee <joyjwlee@gmail.com>
2020-10-08 10:50:51 -0700

    Binary Search

---
+++ 0704. Binary Search.cpp
@@ -0,0 +1,16 @@
+class Solution {
+  public:
+    int search(vector<int> &nums, int target) {
+        int left = 0, right = nums.size() - 1, mid;
+        while (left <= right) {
+            mid = left + (right - left) / 2;
+            if (nums[mid] == target)
+                return mid;
+            if (nums[mid] > target)
+                right = mid - 1;
+            else
+                left = mid + 1;
+        }
+        return -1;
+    }
+};
