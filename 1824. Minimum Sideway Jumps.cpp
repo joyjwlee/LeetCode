e158c1fe553b78ca3c1e2dbd43b31f42723a1f4b
Jaewon Lee <joyjwlee@gmail.com>
2021-04-29 09:32:13 -0700

    Minimum Sideway Jumps

--- 1824. Minimum Sideway Jumps.cpp
+++ 1824. Minimum Sideway Jumps.cpp
@@ -1,18 +1,5 @@
 class Solution {
   public:
-    void printObs(vector<int> &obstacles, vector<vector<int>> &dp) {
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < obstacles.size(); j++) {
-                if (dp[i][j] == 0x3f3f3f3f)
-                    cout << "-1"
-                         << "\t";
-                else
-                    cout << dp[i][j] << "\t";
-            }
-            cout << endl;
-        }
-    }
-
     int minSideJumps(vector<int> &obstacles) {
         // init for dp
         vector<vector<int>> dp(3, vector<int>(obstacles.size(), 0x3f3f3f3f));
@@ -20,13 +7,6 @@ class Solution {
         dp[1][0] = 0;
         dp[2][0] = 1;
         int smallest;
-
-        for (int i : obstacles)
-            cout << i << "\t";
-        cout << endl
-             << endl;
-
-        printObs(obstacles, dp);
 
         // run the dp
         for (int i = 1; i < obstacles.size(); i++) {
@@ -52,11 +32,6 @@ class Solution {
                     dp[j][i] = smallest + 1;
             }
         }
-
-        cout << endl
-             << endl;
-
-        printObs(obstacles, dp);
 
         // find answer and return
         int ans = 0x3f3f3f3f;
