8776e34dc05f610d1b847f9e8d7988910ad9f623
Jaewon Lee <joyjwlee@gmail.com>
2021-06-22 17:58:39 +0900

    Number of Matching Subsequences

--- 0792. Number of Matching Subsequences.cpp
+++ 0792. Number of Matching Subsequences.cpp
@@ -1,5 +1,35 @@
 class Solution {
   public:
+    // vars
+    int i, j, ans;
+    unordered_map<string, int> st;
+
+    bool isSub(string s, string sub) {
+        i = 0, j = 0;
+        for (; i < s.size(); i++) {
+            if (s[i] == sub[j])
+                j++;
+            if (j == sub.size())
+                return true;
+        }
+        return j == sub.size();
+    }
+
+    int numMatchingSubseq(string s, vector<string> &words) {
+        // init
+        ans = 0;
+        for (string str : words)
+            st[str]++;
+
+        // loop through and return
+        for (auto i : st)
+            if (isSub(s, i.first))
+                ans += i.second;
+        return ans;
+    }
+};
+
+    /*
     bool isSub(string s, string sub) {
         int i = 0, j = 0;
         for (; i < s.size(); i++) {
@@ -18,4 +48,4 @@ class Solution {
                 ans++;
         return ans;
     }
-};
+    */
