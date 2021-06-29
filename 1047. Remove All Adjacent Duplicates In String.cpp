04a5d56faa8e4c5cbfc0f99ecdb58b4921baceb9
Jaewon Lee <joyjwlee@gmail.com>
2021-06-29 15:06:07 +0900

    Remove All Adjacent Duplicates In String

--- 1047. Remove All Adjacent Duplicates In String.cpp
+++ 1047. Remove All Adjacent Duplicates In String.cpp
@@ -3,20 +3,22 @@ class Solution {
     string removeDuplicates(string S) {
         // stack solution
         stack<char> s;
-        for (int i = 0; i < S.size(); i++) {
+        for (char c : S) {
             // if empty or not same, add
-            if (s.empty() || s.top() != S[i])
-                s.push(S[i]);
+            if (s.empty() || s.top() != c)
+                s.push(c);
             // if same, remove from stack
             else
                 s.pop();
         }
+
         // return stack as string
         string ans = "";
         while (!s.empty()) {
-            ans = s.top() + ans;
+            ans += s.top();
             s.pop();
         }
+        reverse(ans.begin(), ans.end());
         return ans;
     }
 };
